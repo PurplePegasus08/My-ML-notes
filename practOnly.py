6ca556ae871bd472889543926bad66b88d857486
PurplePegasus08 <anuragrajor400@gmail.com>
2024-10-17 16:00:08 +0530

    Oops

--- practOnly.py
+++ practOnly.py
@@ -73,8 +73,9 @@
 
 
 
-dict1 = {'name': 'Mary', 'age': 22,
-  'student':True,'Country': 'UAE'}
+'''dict1 = {'name': 'Mary', 'age': 22,
+  'student':True,'Country': 'UAE'}'''
+  
 # # for i in dict1.keys():
     
 # #     print(i,end=' ')
@@ -93,13 +94,88 @@ dict1 = {'name': 'Mary', 'age': 22,
 
 
 
-from operator import itemgetter,indexOf
+'''from operator import itemgetter,indexOf
 
 names = [('Ben','Jones'),('Peter','Parker'),
  ('Kelly','Isa')]
 
 sorted_names = sorted(dict1,key=itemgetter(1))
-print(sorted_names)
+print(sorted_names)'''
 
+#--------------------------------------------------------------
+## Atm machine code with OOPs
 
-
+class Atm:
+    def __init__(self):
+        self.pin = ''
+        self.balance = 0
+        self.menu()
+        
+    def menu(self):
+        user_input = input("""
+              How can I help you?
+              1. Press 1 to create pin
+              2. Press 2 to change pin
+              3. Press 3 to check balance
+              4. Press 4 to withdraw balance
+              5. Something else to exit
+        """)
+        if user_input == '1':
+            self.create_pin()
+        elif user_input == '2':
+            self.change_pin()
+        elif user_input == '3':
+            self.check_balance()
+        elif user_input == '4':
+            self.withdraw()
+        else: 
+            exit()
+            
+    def create_pin(self):
+        user_pin = int(input('Enter your pin')) 
+        self.pin = user_pin
+        user_balance = int(input('Enter your Balance'))
+        self.balance = user_balance
+        
+        print('pin created successfully')
+        self.menu()
+    
+    def change_pin(self):
+        old_pin = int(input('enter your old pin'))
+        if old_pin == self.pin:
+            newpin = int(input('Enter your new Pin'))
+            self.pin = newpin
+            print('Pin changed succe---lly')
+        else:
+            print('incorrect pin entered')
+            
+        self.menu()
+        
+    def check_balance(self):
+      user_pin = int(input('Enter the pin'))
+      if user_pin == self.pin:
+        print('your current balance',self.balance)
+        
+      else:
+        print('you entered wrong pin')
+      self.menu()
+    
+    def withdraw(self):
+      user_pin = int(input('Enter the pin'))
+      if user_pin == self.pin:
+        amount = int(input('enter the amount'))
+      if amount <= self.balance:
+        self.balance = self.balance - amount
+        print('withdraw succe---',self.balance)
+        
+      else:
+        print('wrong pin entered')
+    
+      self.menu()
+        
+      
+    
+        
+  
+  
+obj = Atm()
